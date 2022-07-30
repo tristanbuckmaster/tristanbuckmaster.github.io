ede0d557f35f582c3dcb5fd855a22e23c10f89b5
tristanbuckmaster <108429921+tristanbuckmaster@users.noreply.github.com>
2022-07-29 22:05:39 -0400

    shell scripts

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+git checkout --orphan gh-pages
+bundle exec jekyll build
+cd _site
+zip -r site *
+scp site.zip tristanb@grace.umd.edu:
+rm site.zip
+cd ..
+ssh tristanb@grace.umd.edu 'bash -s' < server.sh
---
+++ server.sh
@@ -0,0 +1,3 @@
+rm -r /pub/tristanb/* --force
+unzip site.zip -d /pub/tristanb/
+chmod -R 755 /pub/tristanb/*
